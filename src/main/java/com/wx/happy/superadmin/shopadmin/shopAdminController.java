a571c78a8bb45482dd64d6a8c0ebeb1e6389ac24
tianya20190 <1320249650@qq.com>
2019-04-22 22:59:26 +0800

    2019-4-22

---
+++ src/main/java/com/wx/happy/superadmin/shopadmin/shopAdminController.java
@@ -0,0 +1,14 @@
+package com.wx.happy.superadmin.shopadmin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/shopadmin",method ={RequestMethod.GET})
+public class shopAdminController {
+    @RequestMapping(value = "/shopoperation")
+    public String shopOperation(){
+        return "shop/shopoperation";
+    }
+}
